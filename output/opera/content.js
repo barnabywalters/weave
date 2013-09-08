ff434f6625477665053bc865d48ff4b3536c868c
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-09-08 11:29:02 +0000

    Rebuilt with basic looping through tweets

--- output/opera/content.js
+++ output/opera/content.js
@@ -4,3 +4,15 @@
 // ==/UserScript==
 
 console.log('content script running');
+
+(function (document) {
+	var tweets = document.querySelectorAll('.stream-items .tweet'),
+			tweetEl,
+			tweetText;
+	
+	for (var i=0;i<tweets.length;i++) {
+		tweetEl = tweets[i];
+		tweetText = tweetEl.querySelector('.tweet-text');
+		console.log(tweetText);
+	}
+}(document));
