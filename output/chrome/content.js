611bccac3f805766b052da318f1eb0dd437213be
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-09-08 15:11:11 +0000

    Drafted a bunch of code to do extremely basic syndication verification and content importing

--- output/chrome/content.js
+++ output/chrome/content.js
@@ -26,23 +26,25 @@ console.log('content script running');
 		return str.replace(/#[a-zA-Z0-9]+/i, '');
 	}
 	
-	function urlParser(str) {
-		var parser = document.createElement('a');
-		parser.href = str;
-		return parser;
-	}
-	
 	function isUrl(str) {
-		var parser = urlParser(str);
-		console.log(parser);
-		return parser.scheme && parser.host;
+		var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+		'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+		'((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+		'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+		'(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+		'(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+		if(!pattern.test(str)) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 	
 	function getTrailingUrl(str) {
 		var parts,
 				lastSegment,
 				url;
-		// TODO: fix this, not working
+		
 		parts = str.split(/\s/);
 		parts = parts.filter(function (i) { return i !== ''; });
 		lastSegment = parts.pop();
@@ -52,10 +54,9 @@ console.log('content script running');
 		} else if (lastSegment.charAt(0) === '(' && isUrl(lastSegment.trimChars('()'))) {
 			url = lastSegment.trimChars('()');
 		}
-		console.log(url);
+		
 		if (url !== null && url !== '' && url !== undefined) {
 			url = web_address_to_uri(url, true);
-			console.log(url);
 			return url.trim();
 		}
 
@@ -65,8 +66,38 @@ console.log('content script running');
 	for (var i=0;i<tweets.length;i++) {
 		tweetEl = tweets[i];
 		tweetText = tweetEl.querySelector('.tweet-text');
-		console.log('TEXT IS: ' + stripHashtags(tweetText.innerText));
-		console.log(getTrailingUrl(stripHashtags(tweetText.innerText)));
-		console.log(' ');
+		var tweetUrl = tweetEl; // get URL here
+		var potentialPosseUrl = getTrailingUrl(stripHashtags(tweetText.innerText));
+		
+		if (potentialPosseUrl === null)
+			continue;
+		
+		kango.xhr.send({
+			method: 'GET',
+			url: potentialPosseUrl
+		}, function (result) {
+			// TODO: make this more robust — cast to string and check first char
+			if ([404, 410, 500].indexOf(result.status) && result.response !== null) {
+				console.log('HTTP Request failed', potentialPosseUrl, request);
+				return;
+			}
+			
+			var syndicationLinks = result.response.querySelectorAll('.u-syndication')
+			if (syndicationLinks.length == 0)
+				return;
+			
+			// Check all syndication links to match tweetUrl
+			// TODO: redirects and stuff
+			for (var i=0; i<syndicationLinks.length;i++) {
+				var link = syndicationLinks[i];
+				if (link.href !== tweetUrl)
+					continue;
+				
+				// They match! awesome. Grab the e-content of the original content
+				var originalContent = result.response.querySelector('.h-entry .e-content');
+				tweetText.innerHTML = originalContent.innerHTML;
+				return;
+			}
+		});
 	}
 }(document));
