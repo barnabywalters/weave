1be68e6fc725dee38c55ed238117d61387771a3e
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-09-10 12:34:44 +0100

    Got non-parenthesised replacement of POSSEd copies working, rebuilt

--- src/common/content.js
+++ src/common/content.js
@@ -4,12 +4,8 @@
 // @require cassis.js
 // ==/UserScript==
 
-console.log('content script running');
-
 (function (document) {
-	var tweets = document.querySelectorAll('.stream-items .tweet'),
-			tweetEl,
-			tweetText;
+	var tweets = document.querySelectorAll('.stream-items .tweet');
 	
 	if (!String.prototype.trim) {
 		String.prototype.trim = function () {
@@ -52,7 +48,6 @@ console.log('content script running');
 		parts = str.split(/\s/);
 		parts = parts.filter(function (i) { return i === ')' || i !== ''; });
 		parts = parts.map(function (i) { return i.trimChars('()'); });
-		console.log(parts);
 		
 		lastSegment = parts.pop();
 		
@@ -69,47 +64,45 @@ console.log('content script running');
 	}
 	
 	for (var i=0;i<tweets.length;i++) {
-		tweetEl = tweets[i];
-		tweetText = tweetEl.querySelector('.tweet-text');
-		var tweetUrl = tweetEl; // get URL here
-		var potentialPosseUrl = getTrailingUrl(stripHashtags(tweetText.innerText));
-		
-		if (potentialPosseUrl === null)
-			continue;
-		
-		kango.xhr.send({
-			method: 'GET',
-			url: potentialPosseUrl
-		}, function (result) {
-			// TODO: make this more robust — cast to string and check first char
-			if ([404, 410, 500].indexOf(result.status) !== -1 || result.response === null) {
-				console.log('HTTP Request failed', potentialPosseUrl, result);
+		(function (tweetEl) {
+			var tweetText = tweetEl.querySelector('.tweet-text'),
+					tweetUrl = tweetEl.querySelector('.tweet-timestamp').href,
+					potentialPosseUrl = getTrailingUrl(stripHashtags(tweetText.innerText));
+
+			if (potentialPosseUrl === null)
 				return;
-			}
-			console.log(result);
-			
-			var respDoc = document.implementation.createHTMLDocument('response');
-			respDoc.documentElement.innerHTML = result.response;
-			
-			var syndicationLinks = respDoc.querySelectorAll('.u-syndication')
-			if (syndicationLinks.length === 0)
-				return;
-			
-			console.log('Found syndication links:', syndicationLinks);
-			// Check all syndication links to match tweetUrl
-			// TODO: redirects and stuff
-			for (var i=0; i<syndicationLinks.length;i++) {
-				var link = syndicationLinks[i];
-				if (link.href !== tweetUrl)
-					continue;
+			// TODO: check if domain is in blacklist, ignore if so
+
+			kango.xhr.send({
+				method: 'GET',
+				url: potentialPosseUrl
+			}, function (result) {
+				// TODO: make this more robust — cast to string and check first char
+				if ([404, 410, 500].indexOf(result.status) !== -1 || result.response === null) {
+					console.log('HTTP Request failed', potentialPosseUrl, result);
+					return;
+				}
 				
-				// They match! awesome. Grab the e-content of the original content
-				console.log('Found matching syndication link', link);
-				var originalContent = result.response.querySelector('.h-entry .e-content');
-				console.log('Replacing tweet content with original:', originalContent);
-				tweetText.innerHTML = originalContent.innerHTML;
-				return;
-			}
-		});
+				var respDoc = document.implementation.createHTMLDocument('response');
+				respDoc.documentElement.innerHTML = result.response;
+
+				var syndicationLinks = respDoc.querySelectorAll('.u-syndication')
+				if (syndicationLinks.length === 0)
+					return;
+				
+				// Check all syndication links to match tweetUrl
+				// TODO: redirects and stuff
+				for (var i=0; i<syndicationLinks.length;i++) {
+					var link = syndicationLinks[i];
+					if (link.href !== tweetUrl)
+						continue;
+
+					// They match! awesome. Grab the e-content of the original content
+					var originalContent = respDoc.querySelector('.h-entry .e-content');
+					tweetText.innerHTML = originalContent.innerHTML;
+					return;
+				}
+			});
+		}(tweets[i]));
 	}
 }(document));
